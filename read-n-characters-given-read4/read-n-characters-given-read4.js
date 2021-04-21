20543c778ae5b2c3ccf196595947c2923b17cd0f
Timothy Nguyen <shimoshixp@gmail.com>
2021-04-21 00:35:40 -0700

    Time: 72 ms (93.66%), Space: 38.8 MB (19.51%) - LeetHub

---
+++ read-n-characters-given-read4/read-n-characters-given-read4.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for read4()
+ * 
+ * @param {character[]} buf4 Destination buffer
+ * @return {number} The number of actual characters read
+ * read4 = function(buf4) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} read4()
+ * @return {function}
+ */
+var solution = function(read4) {
+    /**
+     * @param {character[]} buf Destination buffer
+     * @param {number} n Number of characters to read
+     * @return {number} The number of actual characters read
+     */
+    return function(buf, n) {
+      for (let i = 0; i < n; i+=4) {
+        let buf4 = [];
+        read4(buf4);
+        buf.push(...buf4);
+      }
+      
+      buf.splice(n);
+      return buf.length;
+    };
+};
