034017df562e12ded012f76fdb562c386d5534ff
Timothy Nguyen <shimoshixp@gmail.com>
2021-04-22 15:32:47 -0700

    Time: 6672 ms (13.65%), Space: 46.1 MB (97.66%) - LeetHub

---
+++ find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDisappearedNumbers = function(nums) {
+  let result = [];
+  
+  for (let i = 1; i < nums.length + 1; i++) {
+    if (!nums.includes(i)) result.push(i);
+  }
+  
+  return result;
+};
