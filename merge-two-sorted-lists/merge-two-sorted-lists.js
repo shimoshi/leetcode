33bbf9efc2968dc953bad941a756cb87b859c136
Timothy Nguyen <shimoshixp@gmail.com>
2021-04-28 17:19:18 -0700

    Time: 84 ms (91.97%), Space: 40.4 MB (61.71%) - LeetHub

--- merge-two-sorted-lists/merge-two-sorted-lists.js
+++ merge-two-sorted-lists/merge-two-sorted-lists.js
@@ -14,16 +14,8 @@ var mergeTwoLists = function(l1, l2) {
   let list = new ListNode();
   let head = list;
 
-  
-  while (l1 || l2) {
-    if (!l1) {
-      list.next = new ListNode(l2.val);
-      l2 = l2.next;
-    } else if (!l2) {
-      list.next = new ListNode(l1.val);
-      l1 = l1.next;
-    }
-    else if (l1.val <= l2.val) {
+  while (l1 && l2) {
+    if (l1.val <= l2.val) {
       list.next =  new ListNode(l1.val);
       l1 = l1.next;
     } else {
@@ -32,6 +24,12 @@ var mergeTwoLists = function(l1, l2) {
     }
     
     list = list.next;
+  }
+  
+  if (!l1) {
+    list.next = l2;
+  } else if (!l2) {
+    list.next = l1;
   }
   
   return head.next;
