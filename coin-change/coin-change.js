ddf02b4875a6518147b9bb3829ab6178bad8ddfe
Timothy Nguyen <shimoshixp@gmail.com>
2021-05-18 23:31:58 -0700

    Time: 124 ms (68.39%), Space: 42.9 MB (94.66%) - LeetHub

---
+++ coin-change/coin-change.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function(coins, amount, memo = {}, total = 1) {
+  const cache = Array(amount + 1);
+  cache.fill(amount + 1);
+  cache[0] = 0;
+
+  for (let i = 1; i <= amount; i++) {
+    for (let j = 0; j < coins.length; j++) {
+      if (coins[j] <= i) {
+        cache[i] = Math.min(cache[i], cache[i - coins[j]] + 1);
+      }
+    }
+  }
+
+  return cache[amount] > amount ? -1 : cache[amount];
+};
