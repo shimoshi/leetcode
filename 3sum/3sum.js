37b3d8b07cdb21e3e655d50bd4ff0ef36ba454a2
Timothy Nguyen <shimoshixp@gmail.com>
2021-06-17 00:38:37 -0700

    Time: 180 ms (51.37%), Space: 50.1 MB (32.39%) - LeetHub

---
+++ 3sum/3sum.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+  nums.sort((a, b) => a - b);
+  console.log(nums);
+  
+  let result = [];
+  
+  for (let i = 0; i < nums.length - 2 && nums[i] <= 0; i++) {
+    if (nums[i - 1] === nums[i]) {
+      continue;
+    }
+    let low = i + 1;
+    let high = nums.length - 1;
+    
+    while (low < high) {
+      let total = nums[i] + nums[low] + nums[high];
+      if (total < 0) {
+        low++;
+      } else if (total > 0) {
+        high--;
+      } else {
+        result.push([nums[i], nums[low++], nums[high--]]);
+        while (low < high && nums[low] === nums[low - 1]) {
+          low++;
+        }
+      }
+    }
+  }
+  
+  return result;
+};
