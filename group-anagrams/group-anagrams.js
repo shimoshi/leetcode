870209666f1ecaf47946877493d39509ca1a0842
Timothy Nguyen <shimoshixp@gmail.com>
2021-08-24 23:33:43 -0700

    Time: 116 ms (96.10%), Space: 48.2 MB (96.68%) - LeetHub

--- group-anagrams/group-anagrams.js
+++ group-anagrams/group-anagrams.js
@@ -2,31 +2,46 @@
  * @param {string[]} strs
  * @return {string[][]}
  */
+// var groupAnagrams = function(strs) {
+//   const copy = strs.slice();
+//   const result = [];
+//   const sorted = new Array(strs.length);
+  
+//   for (let i = 0; i < strs.length; i++) {
+//     str = strs[i]
+//     sorted[i] = str.split('').sort().join('');
+//   }
+  
+//   while (sorted.length) {
+//     const bucket = [copy.shift()];
+//     let curr = sorted.shift();
+    
+//     for (let k = 0; k < sorted.length; k++) {
+//       if (sorted[k] === curr) {
+//         bucket.push(copy[k]);
+//         copy.splice(k, 1);
+//         sorted.splice(k, 1);
+//         k--
+//       }
+//     }
+    
+//     result.push(bucket);
+//   }
+  
+//   return result;
+// };
+
 var groupAnagrams = function(strs) {
-  const copy = strs.slice();
-  const result = [];
-  const sorted = new Array(strs.length);
+  const map = new Map();
   
-  for (let i = 0; i < strs.length; i++) {
-    str = strs[i]
-    sorted[i] = str.split('').sort().join('');
-  }
-  
-  while (sorted.length) {
-    const bucket = [copy.shift()];
-    let curr = sorted.shift();
-    
-    for (let k = 0; k < sorted.length; k++) {
-      if (sorted[k] === curr) {
-        bucket.push(copy[k]);
-        copy.splice(k, 1);
-        sorted.splice(k, 1);
-        k--
-      }
+  for (let s of strs) {
+    const sorted = s.split('').sort().join();
+    if (map.has(sorted)) {
+      map.set(sorted, [...map.get(sorted), s]);
+    } else {
+      map.set(sorted, [s]);
     }
-    
-    result.push(bucket);
   }
   
-  return result;
-};
+  return [...map.values()];
+}
