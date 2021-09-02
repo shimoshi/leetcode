2fcafbf1b1ab7f4f0016a303a3569aa4a8cc9f91
Timothy Nguyen <shimoshixp@gmail.com>
2021-09-01 22:50:11 -0700

    Time: 92 ms (37.36%), Space: 41.7 MB (24.48%) - LeetHub

---
+++ insert-interval/insert-interval.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[][]} intervals
+ * @param {number[]} newInterval
+ * @return {number[][]}
+ */
+var insert = function(intervals, newInterval) {
+  if (intervals.length === 0) return [newInterval];
+  
+  for (var i = 0; i < intervals.length; i++) {
+    if (newInterval[0] <= intervals[i][0]) {
+      intervals.splice(i, 0, newInterval);
+      break;
+    }
+    
+    if (i === intervals.length - 1) intervals.push(newInterval);
+  }
+  
+  if (i > 0) i--;
+  
+  let isComparing = true;
+  while (i < intervals.length - 1) {
+    let curr = intervals[i];
+    let next = intervals[i + 1];
+    
+    if (curr[1] >= next[0]) {
+      let upper = curr[1] >= next[1] ? curr[1] : next[1];
+      intervals.splice(i, 2, [curr[0], upper]);
+    } else {
+      i++;
+    }
+  }
+  
+  return intervals;
+};
