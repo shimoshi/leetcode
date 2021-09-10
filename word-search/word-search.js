6d05ef993778c868d632263b292cd3fdea30c375
Timothy Nguyen <shimoshixp@gmail.com>
2021-09-10 00:22:35 -0700

    Time: 739 ms (27.55%), Space: 45.3 MB (15.59%) - LeetHub

---
+++ word-search/word-search.js
@@ -0,0 +1,33 @@
+/**
+ * @param {character[][]} board
+ * @param {string} word
+ * @return {boolean}
+ */
+var exist = function(board, word, curr = [0,0]) {
+  if (curr[0] === board.length);
+  
+  for (let r = 0; r < board.length; r++) {
+    for (let c = 0; c < board[r].length; c++) {
+      if (board[r][c] === word[0] && checkAdjacent(r, c, word)) return true;
+    }
+  }
+  
+  return false;
+    
+  function checkAdjacent(row, col, w) {
+    if (board[row][col] !== w[0]) return false;
+    if (w.length === 1) return true;
+    
+    let temp =w[0];
+    board[row][col] = 0;
+    let newW = w.slice(1);
+    
+    if (row - 1 >= 0 && checkAdjacent(row - 1, col, newW)) return true;
+    if (col + 1 < board[0].length && checkAdjacent(row, col + 1, newW)) return true;
+    if (row + 1 < board.length && checkAdjacent(row + 1, col, newW)) return true;
+    if (col - 1 >= 0 && checkAdjacent(row, col - 1, newW)) return true;
+    
+    board[row][col] = temp;
+    return false;
+  }
+};
