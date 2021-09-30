e0375e35d98259acae945ef509e06cee5062888b
Timothy Nguyen <shimoshixp@gmail.com>
2021-09-30 12:01:02 -0700

    Time: 72 ms (94.32%), Space: 40.5 MB (42.10%) - LeetHub

--- two-sum/two-sum.js
+++ two-sum/two-sum.js
@@ -7,11 +7,8 @@ var twoSum = function(nums, target) {
   let vals = {};
 
   for (let i = 0; i < nums.length; i++) {
-    if (target - nums[i] in vals) {
-      return [vals[target-nums[i]], i];
-    } else {
-      vals[nums[i]] = i;
-    }
+    if (target - nums[i] in vals) return [vals[target-nums[i]], i];
+    else vals[nums[i]] = i;
   }
   
   return [];
