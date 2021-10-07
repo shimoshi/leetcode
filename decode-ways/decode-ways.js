76e6b7411036d59d9a5ae97b7c745d446f47f357
Timothy Nguyen <shimoshixp@gmail.com>
2021-10-07 16:08:42 -0700

    Time: 80 ms (68.62%), Space: 39.4 MB (97.48%) - LeetHub

--- decode-ways/decode-ways.js
+++ decode-ways/decode-ways.js
@@ -8,16 +8,16 @@ var numDecodings = function(s) {
   const n = s.length;
   const dp = Array(n+1).fill(0);
 
-  //set base
+  // set base
   dp[0] = 1;
   dp[1] = s[0] === '0' ? 0 : 1;
 
   for (let i = 2; i <= n; i++) {
-    //add current if is a valid code
+    // add current if it is a valid code
     if (s[i - 1] !== '0') {
       dp[i] += dp[i - 1];
     }
-    //add additional for multi-digit codes
+    // add additional for multi-digit codes
     if (s[i - 2] === '1' || s[i - 2] === '2' && s[i - 1] <= '6') {
       dp[i] += dp[i - 2];
     }
