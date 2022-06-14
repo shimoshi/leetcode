481b4ade20aa2f6fa240bf9988c2e38beaccfc77
Timothy Nguyen <timothytamnguyen@gmail.com>
2022-06-14 16:24:16 -0700

    Time: 87 ms (73.80%), Space: 44.4 MB (40.31%) - LeetHub

---
+++ 733-flood-fill/733-flood-fill.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number[][]} image
+ * @param {number} sr
+ * @param {number} sc
+ * @param {number} newColor
+ * @return {number[][]}
+ */
+var floodFill = function(image, sr, sc, newColor) {
+    if (image[sr][sc] === newColor) {
+        return image;
+    }
+    
+    let color = image[sr][sc];
+    image[sr][sc] = newColor;
+    
+    // check up
+    if (sr - 1 >= 0 && image[sr - 1][sc] === color) {
+        floodFill(image, sr - 1, sc, newColor);
+    }
+    
+    // check right
+    if (sc + 1 < image[sr].length && image[sr][sc + 1] === color) {
+        floodFill(image, sr, sc + 1, newColor);
+    }
+    
+    // check down
+    if (sr + 1 < image.length && image[sr + 1][sc] === color) {
+        floodFill(image, sr + 1, sc, newColor);
+    }
+    
+    // check left
+    if (sc - 1 >= 0 && image[sr][sc - 1] === color) {
+        floodFill(image, sr, sc - 1, newColor);
+    }
+    
+    image[sr][sc] = newColor;
+    
+    return image;
+};
