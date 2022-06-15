7365faac64d0a07433e2c52e64c8771e1871044b
Timothy Nguyen <timothytamnguyen@gmail.com>
2022-06-14 17:21:24 -0700

    Time: 108 ms (60.11%), Space: 52.5 MB (16.37%) - LeetHub

---
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function(root, p, q) {
+    if (p === root || q === root) {
+        return root;
+    }
+    
+    if (p.val < root.val && q.val > root.val) {
+        return root;
+    }
+    
+    if (p.val < root.val && q.val < root.val) {
+        return lowestCommonAncestor(root.left, p, q);
+    }
+    
+    if (p.val > root.val && q.val > root.val) {
+        return lowestCommonAncestor(root.right, p, q);
+    }
+
+    return root;
+};
