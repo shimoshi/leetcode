4245c8e6c92ab5edae1563a1ffed87c0b4bb9318
Timothy Nguyen <timothytamnguyen@gmail.com>
2022-06-14 17:01:06 -0700

    Time: 80 ms (96.64%), Space: 50 MB (91.89%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.js
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -3,15 +3,21 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-  let result = nums[0];
-  let temp = nums[0];
-  
-  for (let i = 1; i < nums.length; i++) {
-    if (temp < 0) temp = nums[i];
-    else temp += nums[i];
+    let maxSum = nums[0];
+    let sum = maxSum;
     
-    if (temp > result) result = temp;
-  }
-  
-  return result;
+    
+    for (let i = 1; i < nums.length; i++) {
+        sum += nums[i];
+        
+        if (sum < nums[i]) {
+            sum = nums[i];
+        }
+        
+        if (sum > maxSum) {
+            maxSum = sum;
+        }
+    }
+    
+    return maxSum;
 };
