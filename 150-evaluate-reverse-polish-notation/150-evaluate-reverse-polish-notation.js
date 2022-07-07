0aaa0ec6fde5daf088ced20d75f77f05ff3636eb
Timothy Nguyen <timothytamnguyen@gmail.com>
2022-07-06 21:54:06 -0700

    Time: 107 ms (49.19%), Space: 44.5 MB (80.73%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.js
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.js
@@ -3,31 +3,30 @@
  * @return {number}
  */
 var evalRPN = function(tokens) {
-    const operators = {
-        "*": true,
-        "+": true,
-        "-": true,
-        "/": true,
-    }
+    const map = new Map([
+        ["+", function(a, b) { return a + b}],
+        ["-", function(a, b) { return a - b}],
+        ["*", function(a, b) { return a * b}],
+        ["/", function(a, b) { 
+            a /= b;
+            
+            if (a >= 0) {
+                a = Math.floor(a);
+            } else {
+                a = Math.ceil(a);
+            }
+            
+            return a;
+        }]
+    ]);
     
     const nums = [];
     
     for (const token of tokens) {
-        if (operators[token]) {
-            if (token === "+") {
-                nums[nums.length - 2] += nums.pop();
-            } else if (token === "-") {
-                nums[nums.length - 2] -= nums.pop();
-            } else if (token === "*") {
-                nums[nums.length - 2] *= nums.pop();
-            } else if (token === "/") {
-                nums[nums.length - 2] /= nums.pop();
-                if (nums[nums.length - 1] >= 0) {
-                    nums[nums.length - 1] = Math.floor(nums[nums.length - 1]);
-                } else {
-                    nums[nums.length - 1] = Math.ceil(nums[nums.length - 1])
-                }
-            }
+        if (map.has(token)) {
+            const c2 = nums.pop();
+            const c1 = nums.pop();
+            nums.push(map.get(token)(c1, c2));
         } else {
             nums.push(Number(token));
         }
